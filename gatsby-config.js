fc2a175ad059c4abb5708b88ef3a5baa6dfb07d6
Simon Boeger <simonboeger@Simons-MacBook-Pro-2.local>
2022-03-29 21:01:09 -0700

    added google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,17 @@ module.exports = {
         display: "swap",
       },
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `G-SQYSL9EPEQ`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      }
+    }
   ],
   pathPrefix: "/AdvancedProSystems",
 };
