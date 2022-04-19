858e26e7576d8f341a038896717f1f7386d83966
Jack Boeger <jack@supercleanweb.com>
2022-04-19 13:29:02 -0700

    Update 404.js
    tiny tweaks to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -35,7 +35,7 @@ const NotFoundPage = () => {
         <span role="img" aria-label="Pensive emoji">
           😔
         </span>{" "}
-        Sorry, we cannot find the page you are looking for.
+        we cannot find the page you are looking for.
         <br />
         {process.env.NODE_ENV === "development" ? (
           <>
@@ -45,7 +45,7 @@ const NotFoundPage = () => {
           </>
         ) : null}
         <br />
-        <Link to="/">Go to Advanced Process Systems Corp.</Link>.
+        <Link to="/">Go to Advanced Process Systems Corp</Link>.
       </p>
     </main>
   )
