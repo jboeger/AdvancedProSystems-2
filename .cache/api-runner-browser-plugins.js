6fcb29c96d2fa32256289a89a10f5cc0df2a9bb2
Simon Boeger <simonboeger@Simons-MBP-2.lan>
2023-03-03 23:26:52 -0800

    remove .gitignore folders that were being tracked

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-gtag/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"G-SQYSL9EPEQ","head":true,"anonymize":true},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
