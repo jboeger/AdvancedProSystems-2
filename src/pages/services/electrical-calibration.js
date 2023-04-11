c338a3824685da506a58f64d61b4a063e05242da
Jack Boeger <jack@supercleanweb.com>
2023-04-11 16:25:44 -0700

    Update electrical-calibration.js

--- src/pages/services/electrical-calibration.js
+++ src/pages/services/electrical-calibration.js
@@ -51,15 +51,14 @@ const sections = [
       </p>
       <p>
       <ul>
-      <li><a href="calibrations/">Calibrations - Main Page</a></li>
-      <li><a href="electrical-calibration/">Electrical</a></li>
-      <li><a href="flow-sensor-calibrations/">Flow Sensor</a></li>
+      <li><a href="electrical-calibration/" title="Electrical Calibration" className="link-primary">Electrical</a></li>
+      <li><a href="flow-sensor-calibrations/" title="Flow Sensor Calibration" className="link-primary">Flow Sensor</a></li>
       <li>Mechanical and Balance</li>
       <li>Optical</li>
       <li>Physical Dimensional</li>
       <li>Pipette</li>
-      <li><a href="pressure-calibration/">Pressure</a></li>
-      <li><a href="temperature-calibration/">Temperature / Thermal</a></li>
+      <li><a href="pressure-calibration/" title="Pressure Calibration" className="link-primary">Pressure</a></li>
+      <li><a href="temperature-calibration/" title="Temperature Calibration" className="link-primary">Temperature / Thermal</a></li>
       </ul>
       </p>
       </div>
