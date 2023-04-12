ce502851322413348ae0410fd9f5352d754b2313
Jack Boeger <jack@supercleanweb.com>
2023-04-11 17:10:09 -0700

    Update index.js

--- src/pages/services/index.js
+++ src/pages/services/index.js
@@ -96,9 +96,9 @@ const ServicesPage = () => {
           <div className="col-sm-4">
             <h3><a href="https://www.advancedprosystems.com/services/calibrations/" className="link-primary">Advanced Calibration Services</a></h3>
             <ul>
-              <li><a href="https://www.advancedprosystems.com/services/pressure-calibration/" className="link-primary" title="Pressure Calibration">Pressure</li>
+              <li><a href="https://www.advancedprosystems.com/services/pressure-calibration/" className="link-primary" title="Pressure Calibration">Pressure</a></li>
               <li><a href="https://www.advancedprosystems.com/services/flow-sensor-calibrations/" className="link-primary" title="Flow Sensor Calibrations">Flow</a></li>
-              <li><a href="https://www.advancedprosystems.com/services/electrical-calibration/" className="link-primary" title="Electrical Calibration">Electrical</li>
+              <li><a href="https://www.advancedprosystems.com/services/electrical-calibration/" className="link-primary" title="Electrical Calibration">Electrical</a></li>
               <li>Optical</li>
               <li>Mechanical and Balance</li>
               <li>Dimensional</li>
