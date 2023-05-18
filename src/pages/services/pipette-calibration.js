2b7da38adbc3df6a277a566b3ff7524d075adda2
Jack Boeger <jack@supercleanweb.com>
2023-05-18 10:13:40 -0700

    Create pipette-calibration.js

---
+++ src/pages/services/pipette-calibration.js
@@ -0,0 +1,169 @@
+import * as React from "react";
+import { Section } from "components/Section";
+import { SectionChain } from "components/SectionChain";
+import { Layout } from "components/Layout";
+// import { FAQAccordian } from "../components/homePage/FAQAccordian";
+import { FAQ } from "components/homePage/FAQ";
+
+// image imports
+import heroHomeImg from "images/hero-home-01a-big.jpg";
+import thermometer from "images/thermometer-data-logger-1.png";
+import thermometerglass from "images/glass-thermometer.jpg";
+import thermometerinfrared from "images/infrared-thermometer.jpg";
+import thermometerdata from "images/Thermometer-Data-Logger.png";
+import thermometerlab from "images/thermometers-in-lab.jpg";
+import humidmeter from "images/relative-humidity-and-temperature-meter.jpg";
+
+export const padding = "40px";
+
+const metaData = {
+  title: "Advanced Pipette Calibration | Thermometer, Hygrometer",
+  description: "APS provides Advanced Temperature Calibration Services for hygrometers, digital + analog thermometers, thermocouples, thermal analysis calibration and more!",
+  canonicalUrl: "https://www.advancedprosystems.com/services/pipette-calibration/",
+  img: "",
+  imgAlt: "",
+}
+
+/**
+ * each object (the thing surrounded by {}) in this array is a section.
+ * section properties:
+ *  img: the image. it is refered to not by the url but by an imported object.
+ *  imgAlt: alt for the image 
+ *  imgCols: layout thing. the number of columns allocated to the image.
+ *    there are a total of 12 columns
+ *    for example, if imgCols: "7", the image will take up 7 columns and the text content will take up 5.
+ *  content: text content in the form of html
+ */
+
+const sections = [
+  {
+    img: thermometer,
+    imgAlt: "Thermometer data logger. ",
+    imgCols: "6",
+    content: (
+      <div>
+        <h1>Advanced Pipette Calibration</h1>
+        <p>
+           Adanced Process Systems performs thermometer calibration, thermocouple calibration, thermal cycler calibration, and thermal probe calibration as we can provide thermometer calibration logs, thermometer calibration certificates, and thermal anlysis calibrations.  APS provides precise temperature calibration with our extensive experience in how to calibrate thermocouples, how to calibrate hygrometers, and how to calibrate thermometers. 
+        </p>
+      <div class="list-group-wrapper">
+      <h3><a href="https://www.advancedprosystems.com/services/calibrations/" title="Advanced Calibrations main page" className="link-primary">Advanced Calibrations - Main</a></h3>
+
+      <ul>
+      <li><a href="https://www.advancedprosystems.com/services/electrical-calibration/" title="Electrical Calibration" className="link-primary">Electrical, Multimeters</a></li>
+      <li><a href="https://www.advancedprosystems.com/services/flow-sensor-calibrations/" title="Flow Sensor Calibration" className="link-primary">Flow Rate - Gas, Fluid, Liquid</a></li>
+      <li>Mechanical and Balance</li>
+      <li>Optical</li>
+      <li>Physical Dimensional</li>
+      <li>Pipette</li>
+      <li><a href="https://www.advancedprosystems.com/services/pressure-calibration/" title="Pressure Calibration" className="link-primary">Pressure Gauge Calibration</a></li>
+      <li><strong>Temperature / Thermometers</strong></li>
+      </ul>
+      </div>
+      </div>
+    )
+  },
+    {
+    img: thermometerglass,
+    imgAlt: "Placeholder. ",
+    imgCols: "6",
+    content: (
+      <div>
+        <h2>Digital and glass thermometer calibration services</h2>
+        <p>
+          APS provides expert service in glass thermometer calibration and digital thermometer calibration. We have vast experience in how to calibrate a digital thermometer from a variety of manufacturers. We can perform thermometer calibration compliant to ISO 17025 accreditation. APS is your temperature calibration expert!
+        </p>
+        <h3>Digital and glass thermometer calibration types include:</h3>
+        <ul>
+          <li>APS' thermal range of calibration for digital thermometers is from -196C to 680C</li>
+          <li>APS' thermal range of calibration for glass thermometers is from -50C to 250C</li>
+        </ul>
+        <p>
+          <mark> Need overnight calibration? <a href="https://www.advancedprosystems.com/contact" title="Contact APS" className="link-primary">CONTACT APS</a> for rapid digital thermometer calibration turnaround. </mark>
+        </p> 
+      </div>
+    )
+  },
+  {
+    img: thermometerinfrared,
+    imgAlt: "placeholder. ",
+    imgCols: "6",
+    content: (
+      <div>
+        <h2>Infrared thermometer calibration services</h2>
+        <p>
+          Advanced Process Systems provides expert service in the calibration of infrared thermometers. We have vast experience in how to calibrate infrared thermometers from a variety of manufacturers. We can perform infrared thermometer calibrations compliant to ISO 17025 accreditation.
+        </p>
+      <p>
+          <mark> Need overnight calibration? <a href="https://www.advancedprosystems.com/contact" title="Contact APS" className="link-primary">CONTACT APS</a> for rapid infrared thermometer calibration turnaround. </mark>
+        </p> 
+      </div>
+    )
+  },
+  {
+    img: humidmeter,
+    imgAlt: "placeholder.",
+    imgCols: "6",
+    content: (
+      <div>
+      <h2>Humidity sensor calibration - Hygrometers!</h2>
+        <p>
+          APS provides expert humidity calibration service for hygrometers. We have vast experience in relative humidity calibration standards from a variety of manufacturers. We can perform hygrometer calibration complant to ISO 17025 accreditation for humidity sensor calibration.
+      </p>
+            <p>
+          <mark> Need overnight calibration? <a href="https://www.advancedprosystems.com/contact" title="Contact APS" className="link-primary">CONTACT APS</a> for rapid humidity sensor calibration turnaround. </mark>
+        </p> 
+      </div>
+    )  
+  },
+    {
+    img: thermometerdata,
+    imgAlt: "Placeholder. ",
+    imgCols: "6",
+    content: (
+      <div>
+      <h2>Data logger calibration certificates</h2>
+        <p>
+          APS provides expert data logger calibration service for data logger humidity calibratation. <strong><em>We have vast experience in data logger calibration from all OEM manufacturers</em></strong>. We can provide a data logger calibration certificate compliant to ISO 17025 accreditation.
+       </p>
+            <p>
+          <mark> Need overnight calibration? <a href="https://www.advancedprosystems.com/contact" title="Contact APS" className="link-primary">CONTACT APS</a> for rapid data logger calibration certificate turnaround. </mark>
+        </p> 
+      </div>
+    )  
+  }
+
+  
+]
+
+/**
+ * similar thing here - each object {} is a FAQ entry
+ */
+const faqContent = [
+  {
+    question: "What is temperature calibration?",
+    answer: "Temperature calibration is the process of checking and adjusting the accuracy of instruments used to measure temperature, such as thermometers and temperature sensors. Calibration involves comparing the instrument to a reference standard of known accuracy, and making adjustments to ensure accurate and reliable measurements. Regular calibration is important for meeting industry standards and regulations. "
+  },
+    {
+    question: "What is thermometer calibration?",
+    answer: "Thermometer calibration ensures accurate temperature measurement. Digital thermometers are calibrated by comparing readings to a reference standard, glass thermometers are calibrated at specific temperature points, and infrared thermometers are calibrated against a known reference. Regular calibration is important for meeting industry standards and regulations. "
+  },
+      {
+    question: "How does 'constant temp' affect thermocouple calibration?",
+    answer: "A stable temperature environment is essential during thermocouple calibration to obtain accurate results. Temperature fluctuations can affect the accuracy of the thermocouple readings. A constant temperature is typically achieved using a temperature-controlled furnace or a dry block calibrator. "
+  }
+]
+
+const CalibrationsPage = () => {
+  return (
+    <Layout 
+      metaData={metaData}
+      footerBgColor="rgb(255,255,255,.2)"
+    >
+      <SectionChain content={sections}/>
+      <FAQ title="Temperature Calibration FAQs - Frequently Asked Questions about Thermometer Calibrations" content={faqContent} />
+    </Layout>
+  );
+};
+
+export default CalibrationsPage;
