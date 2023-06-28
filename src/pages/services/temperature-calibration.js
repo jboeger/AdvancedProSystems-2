eb274d80d69872dea808c69e71c00dc758fd11cb
Jack Boeger <jack@supercleanweb.com>
2023-06-28 10:58:41 -0700

    Update temperature-calibration.js

--- src/pages/services/temperature-calibration.js
+++ src/pages/services/temperature-calibration.js
@@ -17,8 +17,8 @@ import humidmeter from "images/relative-humidity-and-temperature-meter.jpg";
 export const padding = "40px";
 
 const metaData = {
-  title: "Advanced Temperature Calibration | Thermometer, Hygrometer",
-  description: "APS provides Advanced Temperature Calibration Services for hygrometers, digital + analog thermometers, thermocouples, thermal analysis calibration and more!",
+  title: "Temperature Calibration Services | Thermometer, Hygrometer",
+  description: "APS provides Advanced Temperature Calibration Services for hygrometers, digital & analog thermometers, thermocouples, thermal analysis calibration and more!",
   canonicalUrl: "https://www.advancedprosystems.com/services/temperature-calibration/",
   img: "https://www.advancedprosystems.com/static/thermometer-data-logger-1-66a2db38fce48b05f6c35497c3f920b6.png",
   imgAlt: "Photo of a thermometer data logger. We calibrate thermometer data loggers. ",
