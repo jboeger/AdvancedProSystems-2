2695ccfa7f4a415630380a4adb552ae737d9e3e5
Jack Boeger <jack@supercleanweb.com>
2023-06-28 10:35:18 -0700

    Update pressure-calibration.js

--- src/pages/services/pressure-calibration.js
+++ src/pages/services/pressure-calibration.js
@@ -48,7 +48,7 @@ const sections = [
            Advanced Process Systems is dedicated to the meticulous calibration of pressure gauge standards, a critical procedure that forms an integral part of our ISO 17025 compliant pressure gauge calibration process. Our extensive knowledge and expertise at APS allow us to proficiently calibrate pressure gauges, employing industry-wide standards for pressure sensor calibration. This proficiency extends to equipment from all manufacturers, reflecting our commitment to universal quality and accuracy.
         </p>
       <div class="list-group-wrapper">
-      <h3><a href="https://www.advancedprosystems.com/services/calibrations/" title="Advanced Calibrations - Main" className="link-primary">Advanced Calibrations - Main</a></h3>
+      <strong><a href="https://www.advancedprosystems.com/services/calibrations/" title="Advanced Calibrations - Main" className="link-primary">Advanced Calibrations - Main</a></strong>
 
       <ul>
       <li><a href="https://www.advancedprosystems.com/services/electrical-calibration/" title="Electrical Calibration" className="link-primary">Electro Calibration, Multimeters</a></li>
