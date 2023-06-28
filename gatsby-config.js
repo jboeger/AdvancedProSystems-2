f61cbb0c4a942d7433cd53b03d3945a7b4287973
Simon Boeger <simonboeger@Simons-MBP-2.attlocal.net>
2023-06-28 14:58:02 -0700

    added sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,56 @@
+const siteUrl = process.env.url || "https://www.advancedprosystems.com";
+
+const pages = [
+  {
+    path: "/",
+    lastmod: "2023-01-01" //format YYYY-MM-DD
+  },
+  {
+    path: "/about",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/contact",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/products",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/",
+    lastmod: "2023-01-01",
+  },
+  {
+    path: "/services/calibrations",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/electrical-calibrations",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/flow-sensor-calibrations",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/iq-oq-pq-validation",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/lab-calibrations",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/pipette-calibration",
+    lastmod: "2023-01-01"
+  },
+  {
+    path: "/services/temperature-calibration",
+    lastmod: "2023-01-01"
+  },
+]
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.advancedprosystems.com",
@@ -29,6 +82,14 @@ module.exports = {
         head: true,
         // enable ip anonymization
         anonymize: true,
+      }
+    },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        resolveSiteUrl: () => siteUrl,
+        resolvePages: () => pages,
+        serialize: ({path, lastmod}) => ({url: path, lastmod: lastmod})
       }
     }
   ],
